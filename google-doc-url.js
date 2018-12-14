5b3d45427479067f708c21f87b8b050afc179021
syouseftaha <43227381+syouseftaha@users.noreply.github.com>
2018-12-14 16:11:33 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1kGVUwIlO5hupSGRq3pRJAzIaAiFxhEE1UFucjOYNMrs/edit#gid=0';
