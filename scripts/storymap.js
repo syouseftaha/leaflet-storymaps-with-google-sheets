0c4de08bbba371c062aa3153ccb151bc07ec1d0c
syouseftaha <43227381+syouseftaha@users.noreply.github.com>
2018-12-14 20:31:53 -0600

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -2,7 +2,7 @@ $(window).on('load', function() {
   var documentSettings = {};
 
   // Some constants, such as default settings
-  const CHAPTER_ZOOM = 15;
+  const CHAPTER_ZOOM = 16;
 
   // This watches for the scrollable container
   var scrollPosition = 0;
