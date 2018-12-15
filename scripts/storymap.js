5ec549884dbe2d308abdab9edcd16ad648442cd2
syouseftaha <43227381+syouseftaha@users.noreply.github.com>
2018-12-14 19:27:32 -0600

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -1,305 +1,163 @@
-$(window).on('load', function() {
-  var documentSettings = {};
+// Watch for scrollable container
+var scrollPosition = 0;
+$('div#contents').scroll(function() {
+  scrollPosition = $(this).scrollTop();
+});
+// Leaflet Map Init
+function initMap() {
+  var map = L.map('map', {
+    zoomControl: false
+  }).setView([0, 0], 5); // initial map automatically fits bounds of all markers
 
-  // Some constants, such as default settings
-  const CHAPTER_ZOOM = 15;
+  // optional : customize link to view source code; add your own GitHub repository
+  map.attributionControl
+  .setPrefix('View <a href="http://github.com/rblades/rblades.github.io">code on GitHub</a>, created with <a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a>');
 
-  // This watches for the scrollable container
-  var scrollPosition = 0;
-  $('div#contents').scroll(function() {
-    scrollPosition = $(this).scrollTop();
+  // optional: add legend to toggle any baselayers and/or overlays
+  // global variable with (null, null) allows indiv layers to be added inside functions below
+  var controlLayers = L.control.layers( null, null, {
+    position: "bottomright",
+    collapsed: false, // false = open by default
+    'data-intro': 'test',
+    'data-position': 'left'
+  }).addTo(map);
+
+  // optional: reposition zoom control other than default topleft
+  L.control.zoom({position: "topright"}).addTo(map);
+
+  /* BASELAYERS */
+  // use common baselayers below, delete, or add more with plain JavaScript from http://leaflet-extras.github.io/leaflet-providers/preview/
+  // .addTo(map); -- suffix displays baselayer by default
+  // controlLayers.addBaseLayer (variableName, 'label'); -- adds baselayer and label to legend; omit if only one baselayer with no toggle desired
+  // http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png
+  var lightAll = new L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    attribution: '<a href="http://maps.stamen.com/toner/#12/37.7706/-122.3782/</a>'
+  }).addTo(map); //this displays layer by default
+  controlLayers.addBaseLayer(lightAll, 'Pembroke Today');
+
+  // Esri satellite map from http://leaflet-extras.github.io/leaflet-providers/preview/
+  var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
   });
+  controlLayers.addBaseLayer(Esri_WorldImagery, 'Pembroke Satellite');
 
-  /**
-   * Triggers the load of the spreadsheet and map creation
-   */
-   var mapData;
+  // Harvard Map Warper Pemb 1970
+  var Pemb1967 = new L.tileLayer('http://warp.worldmap.harvard.edu/maps/tile/5310/{z}/{x}/{y}.png', {
+    attribution: '<a href="http://warp.worldmap.harvard.edu/</a>'
+  });
+  controlLayers.addOverlay(Pemb1967, 'Pembroke 1967');
 
-   $.ajax({
-     url:'csv/Options.csv',
-     type:'HEAD',
-     error: function() {
-       // Options.csv does not exist, so use Tabletop to fetch data from
-       // the Google sheet
-       mapData = Tabletop.init({
-         key: googleDocURL,
-         callback: function(data, mapData) { initMap(); }
-       });
-     },
-     success: function() {
-       // Get all data from .csv files
-       mapData = Procsv;
-       mapData.load({
-         self: mapData,
-         tabs: ['Options', 'Chapters'],
-         callback: initMap
-       });
-     }
-   });
+  var Pemb1980 = new L.tileLayer('http://warp.worldmap.harvard.edu/maps/tile/5312/{z}/{x}/{y}.png', {
+    attribution: '<a href="http://warp.worldmap.hardvard.edu/</a>'
+  });
+  controlLayers.addOverlay(Pemb1980, 'Pembroke 1980');
 
-  /**
-  * Reformulates documentSettings as a dictionary, e.g.
-  * {"webpageTitle": "Leaflet Boilerplate", "infoPopupText": "Stuff"}
-  */
-  function createDocumentSettings(settings) {
-    for (var i in settings) {
-      var setting = settings[i];
-      documentSettings[setting.Setting] = setting.Customize;
-    }
-  }
+  // Harvard Map Warper Pemb 1967
+  //var Pemb1967 = new L.tileLayer('http://warp.worldmap.harvard.edu/maps/tile/5310/{z}/{x}/{y}.png', {
+   // attribution: '<a href="http://warp.worldmap.harvard.edu/</a>'
+  //});
+  //controlLayers.addOverlay(Pemb1967, 'Pembroke 1967');
 
-  /**
-   * Returns the value of a setting s
-   * getSetting(s) is equivalent to documentSettings[constants.s]
-   */
-  function getSetting(s) {
-    return documentSettings[constants[s]];
-  }
+// Opacity Slider
+  var handle = document.getElementById('handle'),
+    start = false,
+    startTop;
 
-  /**
-   * Returns the value of setting named s from constants.js
-   * or def if setting is either not set or does not exist
-   * Both arguments are strings
-   * e.g. trySetting('_authorName', 'No Author')
-   */
-  function trySetting(s, def) {
-    s = getSetting(s);
-    if (!s || s.trim() === '') { return def; }
-    return s;
-  }
+  document.onmousemove = function(e) {
+      if (!start) return;
+      // Adjust control.
+      handle.style.top = Math.max(-5, Math.min(195, startTop + parseInt(e.clientY, 10) - start)) + 'px';
+      // Adjust opacity.
+      Pemb1967.setOpacity(1 - (handle.offsetTop / 200));
+      Pemb1980.setOpacity(1 - (handle.offsetTop / 200));
+  };
 
-  /**
-   * Loads the basemap and adds it to the map
-   */
-  function addBaseMap() {
-    var basemap = trySetting('_tileProvider', 'Stamen.TonerLite');
-    L.tileLayer.provider(basemap, {
-      maxZoom: 18,
-      minZoom: 8,
-    }).addTo(map);
-  }
+  handle.onmousedown = function(e) {
+      // Record initial positions.
+      start = parseInt(e.clientY, 10);
+      startTop = handle.offsetTop - 5;
+      return false;
+  };
 
-  function initMap() {
-    var options = mapData.sheets(constants.optionsSheetName).elements;
-    createDocumentSettings(options);
+  document.onmouseup = function(e) {
+      start = null;
+  };
 
-    /* Change narrative width */
-    narrativeWidth = parseInt(getSetting('_narrativeWidth'));
-    if (narrativeWidth > 0 && narrativeWidth < 100) {
-      var mapWidth = 100 - narrativeWidth;
+  // *GET THE GEOJSON
+  $.getJSON('data.geojson', function(data) {
+    var geojson = L.geoJson(data, {
+      onEachFeature: function (feature, layer) {
+        (function(layer, properties) {
+          // Create numerical icons to match the ID numbers
+          // OR remove the next 6 lines for default blue Leaflet markers
+          var numericMarker = L.ExtraMarkers.icon({
+            icon: 'fa-number',
+            number: feature.properties['id'],
+            markerColor: 'blue'
+          });
+          layer.setIcon(numericMarker);
 
-      $('#narration, #title').css('width', narrativeWidth + 'vw');
-      $('#map').css('width', mapWidth + 'vw');
-    }
-
-    var chapterContainerMargin = 70;
-
-    document.title = getSetting('_mapTitle');
-    $('#title').append('<h3>' + getSetting('_mapTitle') + '</h3>');
-    $('#title').append('<small>' + getSetting('_mapSubtitle') + '</small>');
-
-    // Load tiles
-    addBaseMap();
-
-    // Add zoom controls if needed
-    if (getSetting('_zoomControls') !== 'off') {
-      L.control.zoom({
-        position: getSetting('_zoomControls')
-      }).addTo(map);
-    }
-
-    var chapters = mapData.sheets(constants.chaptersSheetName).elements;
-
-    var markers = [];
-    var pixelsAbove = [];
-    var chapterCount = 0;
-
-    var currentlyInFocus; // integer to specify each chapter is currently in focus
-    var overlay;  // URL of the overlay for in-focus chapter
-
-    for (i in chapters) {
-      var c = chapters[i];
-
-      if ( !isNaN(parseFloat(c['Latitude'])) && !isNaN(parseFloat(c['Longitude']))) {
-        var lat = parseFloat(c['Latitude']);
-        var lon = parseFloat(c['Longitude']);
-
-        markers.push(
-          L.marker([lat, lon], {
-            icon: L.ExtraMarkers.icon({
-              icon: 'fa-number',
-              number: ++chapterCount,
-              markerColor: 'blue'
-            })
-          }
-        ));
-
-      } else {
-        markers.push(null);
-      }
-
-      var image = $('<img>', {
-        src: c['Image Link'],
-      });
-
-      var source = $('<a>', {
-        text: c['Image Credit'],
-        href: c['Image Credit Link'],
-        target: "_blank",
-        class: 'source'
-      });
-
-      var container = $('<div></div>', {
-        id: 'container' + i,
-        class: 'chapter-container'
-      });
-
-      var imgContainer = $('<div></div', {
-        class: 'img-container'
-      }).append(image);
-
-      container
-        .append('<p class="chapter-header">' + c['Chapter'] + '</p>')
-        .append(imgContainer)
-        .append(source)
-        .append('<p class="description">' + c['Description'] + '</p>');
-
-      $('#contents').append(container);
-    }
-
-    changeAttribution();
-
-    /* Change image container heights */
-    imgContainerHeight = parseInt(getSetting('_imgContainerHeight'));
-    if (imgContainerHeight > 0) {
-      $('.img-container').css({
-        'height': imgContainerHeight + 'px',
-        'max-height': imgContainerHeight + 'px',
-      });
-    }
-
-    // For each block (chapter), calculate how many pixels above it
-    pixelsAbove[0] = -100;
-    for (i = 1; i < chapters.length; i++) {
-      pixelsAbove[i] = pixelsAbove[i-1] + $('div#container' + (i-1)).height() + chapterContainerMargin;
-    }
-    pixelsAbove.push(Number.MAX_VALUE);
-
-    $('div#contents').scroll(function() {
-      var currentPosition = $(this).scrollTop();
-      for (i = 0; i < pixelsAbove.length - 1; i++) {
-        if (currentPosition >= pixelsAbove[i] && currentPosition < (pixelsAbove[i+1] - 2 * chapterContainerMargin) && currentlyInFocus != i) {
-          // Remove styling for the old in-focus chapter and
-          // add it to the new active chapter
-          $('.chapter-container').removeClass("in-focus").addClass("out-focus");
-          $('div#container' + i).addClass("in-focus").removeClass("out-focus");
-
-          currentlyInFocus = i;
-
-          // Remove overlay tile layer if needed
-          if (map.hasLayer(overlay)) {
-            map.removeLayer(overlay);
-          }
-
-          // Add chapter's overlay tiles if specified in options
-          if (chapters[i]['Overlay'] != '') {
-            var opacity = (chapters[i]['Overlay Transparency'] != '') ? parseFloat(chapters[i]['Overlay Transparency']) : 1;
-            overlay = L.tileLayer(chapters[i]['Overlay'], {opacity: opacity});
-            overlay.addTo(map);
-          }
-
-          // Fly to the new marker destination if latitude and longitude exist
-          if (chapters[i]['Latitude'] && chapters[i]['Longitude']) {
-            var zoom = chapters[i]['Zoom'] ? chapters[i]['Zoom'] : CHAPTER_ZOOM;
-            map.flyTo([chapters[i]['Latitude'], chapters[i]['Longitude']], zoom);
-          }
-
-          // No need to iterate through the following chapters
-          break;
-        }
+          // Create the contents of each chapter
+          var chapter = $('<p></p>', {
+            text: feature.properties['chapter'],
+            class: 'chapter-header'
+          });
+          var image = $('<img>', {
+            src: feature.properties['image'],
+            width: '100%'
+          });
+          // I am using 'var source' to link out to my timeline pages. Orginally used to link to source of image
+          var source = $('<a>', {
+            text: feature.properties['source-credit'],
+            href: feature.properties['source-link'],
+            target: "_blank",
+            class: 'source'
+          });
+          // Added description to add above audio controls to describe audio
+          var audioDescription = $('<p></p>', {
+            text: feature.properties['audioDescription'],
+            class: 'audio-text'
+          });
+          // Added audio controls to play audio at each marker
+          var audio = $('<audio></audio>', {
+            src: feature.properties['audio'],
+            controls: 'controls',
+            preload: 'auto',
+          });
+          var description = $('<p></p>', {
+            text: feature.properties['description'],
+            class: 'description'
+          });
+          var container = $('<div></div>', {
+            id: 'container' + feature.properties['id'],
+            class: 'image-container'
+          });
+          container.append(chapter).append(image).append(source).append(audioDescription).append(audio).append(description);
+          $('#contents').append(container);
+          // Watch the current scroll postion for scroll-driven map navigation!
+          var areaHeight = $('.image-container').height() + 50;
+          var areaTop = (feature.properties['id']-1) * areaHeight - 50; // -50 is a minor adjustment
+          var areaBottom = areaTop + areaHeight - 50; // -50 is a minor adjustment
+          $('div#contents').scroll(function() {
+            if($(this).scrollTop() >= areaTop && $(this).scrollTop() < areaBottom) {
+              $('.image-container').css('opacity', 0.3);
+              $('div#container' + feature.properties['id']).css('opacity', 1);
+              map.flyTo([feature.geometry.coordinates[1], feature.geometry.coordinates[0] ], feature.properties['zoom']);
+            }
+          });
+//          $(numericMarker).click(function(e) {
+//            if($(this) != $('div#container' + feature.properties['id']).css('opacity', 1)) {
+//              $('div#contents').animate({
+//              scrollTop: $('div#container' + feature.properties['id']).offset().top},
+//                'slow')
+//            };
+//          });
+        })(layer, feature.properties);
       }
     });
-
-
-    $('#contents').append(" \
-      <div id='space-at-the-bottom'> \
-        <a href='#space-at-the-top'>  \
-          <i class='fa fa-chevron-up'></i></br> \
-          <small>Top</small>  \
-        </a> \
-      </div> \
-    ");
-
-    /* Generate a CSS sheet with cosmetic changes */
-    $("<style>")
-      .prop("type", "text/css")
-      .html("\
-      #narration, #title {\
-        background-color: " + trySetting('_narrativeBackground', 'white') + "; \
-        color: " + trySetting('_narrativeText', 'black') + "; \
-      }\
-      a, a:visited, a:hover {\
-        color: " + trySetting('_narrativeLink', 'blue') + " \
-      }\
-      .in-focus {\
-        background-color: " + trySetting('_narrativeActive', '#f0f0f0') + " \
-      }")
-      .appendTo("head");
-
-
-    endPixels = parseInt(getSetting('_pixelsAfterFinalChapter'));
-    if (endPixels > 100) {
-      $('#space-at-the-bottom').css({
-        'height': (endPixels / 2) + 'px',
-        'padding-top': (endPixels / 2) + 'px',
-      });
-    }
-
-    var bounds = [];
-    for (i in markers) {
-      if (markers[i]) {
-        markers[i].addTo(map);
-        markers[i]['_pixelsAbove'] = pixelsAbove[i];
-        markers[i].on('click', function() {
-          var pixels = parseInt($(this)[0]['_pixelsAbove']) + 5;
-          $('div#contents').animate({
-            scrollTop: pixels + 'px'});
-        });
-        bounds.push(markers[i].getLatLng());
-      }
-    }
-    map.fitBounds(bounds);
-
-    $('#map, #narration, #title').css('visibility', 'visible');
-    $('div.loader').css('visibility', 'hidden');
-
-    $('div#container0').addClass("in-focus");
-    $('div#contents').animate({scrollTop: '1px'});
-  }
-
-
-  /**
-   * Changes map attribution (author, GitHub repo, email etc.) in bottom-right
-   */
-  function changeAttribution() {
-    var attributionHTML = $('.leaflet-control-attribution')[0].innerHTML;
-    var credit = 'View <a href="' + googleDocURL + '" target="_blank">data</a>';
-    var name = getSetting('_authorName');
-    var url = getSetting('_authorURL');
-
-    if (name && url) {
-      if (url.indexOf('@') > 0) { url = 'mailto:' + url; }
-      credit += ' by <a href="' + url + '">' + name + '</a> | ';
-    } else if (name) {
-      credit += ' by ' + name + ' | ';
-    } else {
-      credit += ' | ';
-    }
-
-    credit += 'View <a href="' + getSetting('_githubRepo') + '">code</a>';
-    if (getSetting('_codeCredit')) credit += ' by ' + getSetting('_codeCredit');
-    credit += ' with ';
-    $('.leaflet-control-attribution')[0].innerHTML = credit + attributionHTML;
-  }
-
-});
+    $('div#container1').css('opacity', 1);
+    map.fitBounds(geojson.getBounds());
+    geojson.addTo(map);
+  });
+}
+initMap();
